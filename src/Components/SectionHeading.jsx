3d81ec1ef35254bf309640a9b1d2c56b415f1fa8
ayayehia <ayanader8@gmail.com>
2023-12-12 16:40:49 +0200

    updates in the contactUs Page

---
+++ src/Components/SectionHeading.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SectionHeading = () => {
+  return (
+    <article className="flex flex-col justify-start z-50 mt-[20%] md:mt-[15%] p-8 gap-4 lg:w-1/2">
+      <h3 className="text-[#ee626b] text-md font-bold">|Contact Us</h3>
+      <h2 className="uppercase font-extrabold text-2xl">
+        Get In Touch with our Agents
+      </h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit asperiores
+        ducimus quam vero, non minus nisi inventore, nihil consectetur sed
+        pariatur beatae modi? Dolore voluptate eligendi, laudantium odio
+        voluptates a non consequuntur aspernatur nisi vitae reiciendis deleniti
+        ab magni distinctio ullam totam, expedita aliquid molestiae et! Officiis
+        porro minima ad?
+      </p>
+    </article>
+  );
+};
+
+export default SectionHeading;
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -4,15 +4,17 @@ import Form from "../Components/Contact/Form";
 import Footer from "../Components/Footer";
 import PageHeading from "../Components/Shared/PageHeading";
 import Image from "../assets/Images/contactus-medium.jpg";
+import SectionHeading from "../Components/SectionHeading";
 const ContactUs = () => {
   return (
-    <section className=" flex flex-col  gap-8">
+    <section className=" flex flex-col  gap-4 ">
       <PageHeading
         title={"Contact Us"}
         tagline={"Home/Contact Us"}
         image={Image}
       />
-      <section className="lg:flex justify-around items-center h-full mt-20  lg:mt-[12rem] 2xl:mt-[23%]">
+      <SectionHeading />
+      <section className="lg:flex justify-around items-center h-full ">
         <span className="lg:w-[60%]">
           <ContactInfo />
         </span>
